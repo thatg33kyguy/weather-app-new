7a93a71e5daba182cd13965603aa077ef5abf9f8
Rajprateem <rajprateemnath101@gmail.com>
2025-08-05 16:59:05 +0530

    Update API URL to production

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080'; // Adjust to your backend URL
+// const API_BASE_URL = 'http://localhost:8080'; // Adjust to your backend URL
+const API_BASE_URL = 'https://weather-app-kebd.onrender.com';
+
 
 export const getDailySummaries = (city) => axios.get(`${API_BASE_URL}/api/dailySummary/${city}`);
 export const setThresholds = (thresholds) => axios.post(`${API_BASE_URL}/api/alertThresholds`, thresholds);
