e18cd054bfacd0311ac9e181cb081bf0d76860e0
Rajprateem <rajprateemnath101@gmail.com>
2025-08-07 18:11:31 +0530

    awawe

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'http://localhost:8080'; 
 // const API_BASE_URL = 'https://weather-app-kebd.onrender.com';
 
-export const getDailySummaries = (city) =>
-  axios.get(`${API_BASE_URL}/api/dailySummary/${city}`, { withCredentials: true });
 
-export const setThresholds = (thresholds) =>
-  axios.post(`${API_BASE_URL}/api/alertThresholds`, thresholds, { withCredentials: true });
-
-export const getAlertThresholds = () =>
-  axios.get(`${API_BASE_URL}/api/alertThresholds`, { withCredentials: true });
+export const getDailySummaries = (city) => axios.get(`${API_BASE_URL}/api/dailySummary/${city}`);
+export const setThresholds = (thresholds) => axios.post(`${API_BASE_URL}/api/alertThresholds`, thresholds);
+export const getAlertThresholds = () => axios.get(`${API_BASE_URL}/api/alertThresholds`);
+// Removed the getTriggeredAlerts function
