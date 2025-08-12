54a049644db8448b4e6d95435e05b7c7e310a0a8
Rajprateem Nath <122095887+thatg33kyguy@users.noreply.github.com>
2025-08-12 16:26:58 +0530

    Rename file to lowercase dailySummary.js

--- server/Routes/dailySummary.js
+++ server/Routes/dailySummary.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const DailySummary = require('../schema/dailySummary'); // keep PascalCase since it's a model
+const DailySummary = require('./schema/DailySummary'); // keep PascalCase since it's a model
 
 // GET all daily summaries
 router.get('/', async (req, res) => {
