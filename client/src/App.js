12d931b669933e703924053464f6ef6e97ad9c47
Rajprateem Nath <122095887+thatg33kyguy@users.noreply.github.com>
2025-08-12 20:03:33 +0530

    Restore client folder after cleanup

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DailySummary from './components/DailySummary';
+import HistoricalTrends from './components/HisotricalTrends';
+import Navbar from './components/NavBar';
+import SetThresholds from './components/setThresholds';
+import './App.css'
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<DailySummary />} />
+          <Route path="/historical-trends" element={<HistoricalTrends />} />
+          <Route path="/set-thresholds" element={<SetThresholds />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ client/src/components/DailySummary.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { getDailySummaries } from '../api';
+import './DailySummary.css'; // Import the CSS file
+
+const cities = ['Delhi', 'Mumbai', 'Chennai', 'Bangalore', 'Kolkata', 'Hyderabad'];
+
+const DailySummary = () => {
+  const [summaries, setSummaries] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSummaries = async () => {
+      try {
+        const summariesData = {};
+
+        // Fetch summaries for each city
+        for (const city of cities) {
+          const response = await getDailySummaries(city);
+          summariesData[city] = response.data;
+        }
+
+        setSummaries(summariesData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching daily summaries:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchSummaries();
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="header">Daily Weather Summaries</h2>
+      {loading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        Object.keys(summaries).map(city => (
+          <div key={city} className="city-container">
+            <h3>{city}</h3>
+            <ul className="summary-list">
+              {summaries[city].map((summary, index) => (
+                <li key={index}>
+                  {`${summary.date}: Avg Temp ${summary.avg_temp}°C, Max Temp ${summary.max_temp}°C, Min Temp ${summary.min_temp}°C, Condition ${summary.dominant_condition}`}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default DailySummary;
