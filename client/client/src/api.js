8fca6d4d5aefaabd490c238cb94249b6478023a3
Rajprateem Nath <122095887+thatg33kyguy@users.noreply.github.com>
2025-08-12 19:20:12 +0530

    k

--- client/client/src/api.js
+++ client/client/src/api.js
@@ -2,12 +2,6 @@ import axios from 'axios';
 
 // Read API base URL from environment variable
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-<<<<<<< HEAD
-=======
-=======
-// const API_BASE_URL = 'http://localhost:8080'; 
-
->>>>>>> e7cce86749cace6d00bc9a0a84e3403b3dad8d18
 
 // API calls
 export const getDailySummaries = (city) =>
