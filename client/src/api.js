e7cce86749cace6d00bc9a0a84e3403b3dad8d18
Rajprateem Nath <122095887+thatg33kyguy@users.noreply.github.com>
2025-08-12 18:23:01 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -5,8 +5,7 @@ import axios from 'axios';
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 =======
 // const API_BASE_URL = 'http://localhost:8080'; 
-const API_BASE_URL = 'https://weather-app-kebd.onrender.com';
->>>>>>> 5fc63aadee21548fc3021e9a674889187defaaa8
+
 
 // API calls
 export const getDailySummaries = (city) =>
